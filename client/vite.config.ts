d731c33b7890ea7b7b1e3549b0dd3f2f1cfc8cc6
Aleksander Korkh <korkh1979@gmail.com>
2024-04-07 15:02:21 +0200

    Vite cinit config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(() => {
+  return {
+      build: {
+          outDir: '../API/wwwroot'
+      },
+      server: {
+          port: 3000
+      },
+      plugins: [react()]
+  }
 })
