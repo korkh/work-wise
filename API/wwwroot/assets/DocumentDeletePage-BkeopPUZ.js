0c3a9d23ac213e1d25aaaca397b804c9504c71ca
Aleksander Korkh <korkh1979@gmail.com>
2024-06-20 10:19:58 +0200

    Drawer bg color correction, build uodated

---
+++ API/wwwroot/assets/DocumentDeletePage-BkeopPUZ.js
@@ -0,0 +1,1 @@
+import{r as a,d as o,k as c,m as n}from"./index-DI3RA6yi.js";const m="_documentDeletePage_1qgsh_1",l={documentDeletePage:m},r=e=>{const{className:t}=e,{t:s}=o();return c.jsx("div",{className:n(l.documentDeletePage,[t],{}),children:s("DocumentDeletePage")})},D=a.memo(r);export{D as default};
